5c31b550c6a8ea248ae6a9ab0854e8766a3eda2a
tonopurwanto <tono.purwanto@live.com>
2017-08-03 16:03:46 +0700

    -

---
+++ app/src/main/java/tonopurwanto/lockapp/DeviceAdminReceiver.java
@@ -0,0 +1,13 @@
+package tonopurwanto.lockapp;
+
+import android.content.ComponentName;
+import android.content.Context;
+
+public class DeviceAdminReceiver extends android.app.admin.DeviceAdminReceiver
+{
+    private static final String TAG = "DeviceAdminReceiver";
+
+    public static ComponentName getComponentName(Context context) {
+        return new ComponentName(context.getApplicationContext(),  DeviceAdminReceiver.class);
+    }
+}
